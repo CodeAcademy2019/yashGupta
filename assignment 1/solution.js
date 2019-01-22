a19ed72aaa6903fd662ecd79ed520fab105d457a
Yash <guptayash955@gmail.com>
2019-01-22 12:17:40 +0530

    Adding edge cases and logic modification to the code. Linting done

--- assignment 1/solution.js
+++ assignment 1/solution.js
@@ -3,49 +3,37 @@ const roll = (array) => {
   let strike = false;
   let spare = false;
   let frame = 1;
-  // const reducer = (val, val1) => {
-  //   if (val==10)
-  //   console.log(val, val1)
-  //   if (val+val1=== 10){
-
-  //   }
-  // }
-
-  // array.reduce(reducer);
   for (let i = 0; i < array.length; i += 2) {
-    // console.log (array[i], array[i+1]);
     if (array[i] === 10) {
       strike = true;
-      // console.log("yayy Strike")
-      // console.log(array[i], array[i+1],total, array[i+2])
       total = total + array[i] + array[i + 1] + array[i + 2];
       if (frame === 10) {
         break;
       }
-      console.log(total);
     }
     if (array[i] + array[i + 1] === 10 && (strike === false)) {
       spare = true;
-      // console.log("yayy spare")
       total = total + array[i] + array[i + 1] + array[i + 2];
+
+      if (frame === 10) {
+        break;
+      }
     }
     if ((strike === false) && (spare === false)) {
-      // console.log("yayy else",  array[i], array[i+1]);
       total = total + array[i] + array[i + 1];
     }
     frame += 1;
-    // console.log(total);
     if (strike === true) {
       i -= 1;
     }
-    strike = false;
+    strike = false; spare = false;
   }
+  if (frame < 10) {
+    return 'Game Quit.';
+  }
+
   return (total);
 };
 
-// const score = () => {
-
-// }
-
-roll([0, 0, 0, 0, 0, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-module.exports = { roll };
+const score = array => roll(array);
+module.exports = { roll, score };
--- assignment 1/solution.test.js
+++ assignment 1/solution.test.js
@@ -1,17 +1,42 @@
 const bowl = require('./solution.js');
 
 describe('roll', () => {
-  it ('should calculate the score for each throw' , () => {
+  it('should calculate the score for each throw', () => {
     expect(bowl.roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toEqual(90);
-  })
+  });
 
-  it ('can take array of length more than 20 (when strike appears in 10th frame)' , () => {
+  it('can take array of length more than 20 (when strike appears in 10th frame)', () => {
     expect(bowl.roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toEqual(30);
-  })
+  });
 
-  it ('should return 0 when every roll has a zero pin knocked out.' , () => {
-    expect (bowl.roll([ 0, 0, 0, 0, 0, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ])).toEqual(30);
-  })
+  it('should return 0 when every roll has a zero pin knocked out.', () => {
+    expect(bowl.roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual(0);
+  });
+
+  it('should return', () => {
+    expect(bowl.roll([0, 0, 0, 0, 0, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual(30);
+  });
+
+  it('should return the max score one can reach in 12 strikes in a row', () => {
+    expect(bowl.roll([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])).toEqual(300);
+  });
+
+  it('should', () => {
+    expect(bowl.roll([10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10])).toEqual(120);
+  });
+
+  it('should check for spare ', () => {
+    expect(bowl.roll([4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 10])).toEqual(146);
+  });
 
   // xit ('should return ')
-})
+});
+
+describe('score', () => {
+  it('should return the total score of the game', () => {
+    expect(bowl.score([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toEqual(16);
+  });
+  it('should return "Game Quit" if enough inputs are not provided', () => {
+    expect(bowl.score([1, 2])).toEqual('Game Quit.');
+  });
+});
