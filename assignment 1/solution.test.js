0c1c1201fa7e633e8507b31a228c9831679dd3ac
Yash <guptayash955@gmail.com>
2019-01-22 11:18:07 +0530

    first commit for solution file, test cases working

--- assignment 1/solution.test.js
+++ assignment 1/solution.test.js
@@ -4,4 +4,14 @@ describe('roll', () => {
   it ('should calculate the score for each throw' , () => {
     expect(bowl.roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toEqual(90);
   })
+
+  it ('can take array of length more than 20 (when strike appears in 10th frame)' , () => {
+    expect(bowl.roll([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toEqual(30);
+  })
+
+  it ('should return 0 when every roll has a zero pin knocked out.' , () => {
+    expect (bowl.roll([ 0, 0, 0, 0, 0, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ])).toEqual(30);
+  })
+
+  // xit ('should return ')
 })
